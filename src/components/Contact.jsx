dbb2c939e49f7b0c116ddacdccd96fd75fbb4fb5
Harshda95 <harshdagargi@gmail.com>
2025-08-21 13:21:01 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import { Mail, Phone, MapPin } from "lucide-react";
 
 const Contact = () => {
   return (
-    <section id="contact" className="py-15 bg-gray-50 dark:bg-gray-900 px-15">
+    <section id="contact" className="py-15 bg-gray-50 dark:bg-gray-900 ">
       <div className="container mx-auto px-6">
         <h2 className="text-4xl font-bold text-center mb-14 text-gray-900 dark:text-white">
           Contact <span className="text-blue-600 dark:text-blue-400">Us</span>
