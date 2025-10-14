be8b50c3567d27ff9a49b5b4b451c7e47d513261
Raiyan Hasan <raiyanhasan2006@gmail.com>
2025-10-14 15:40:02 +0530

    feat: add ESLint-Prettier integration for enhanced code quality

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
@@ -12,6 +14,7 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -22,8 +25,12 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    plugins: {
+      prettier: prettier,
+    },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'prettier/prettier': 'error',
     },
   },
 ]);
